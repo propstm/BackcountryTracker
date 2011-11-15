95b5bf6f11ee2829e5e399409265a37488b6e4e5
Matthew Propst <propstm@Matthews-MacBook-Air.local>
2011-11-14 20:16:03 -0500

    Initial Commit

---
+++ backcountrytrackerTests/backcountrytrackerTests.h
@@ -0,0 +1,13 @@
+//
+//  backcountrytrackerTests.h
+//  backcountrytrackerTests
+//
+//  Created by Matthew Propst on 11/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface backcountrytrackerTests : SenTestCase
+
+@end
---
+++ backcountrytrackerTests/backcountrytrackerTests.m
@@ -0,0 +1,32 @@
+//
+//  backcountrytrackerTests.m
+//  backcountrytrackerTests
+//
+//  Created by Matthew Propst on 11/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "backcountrytrackerTests.h"
+
+@implementation backcountrytrackerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in backcountrytrackerTests");
+}
+
+@end
